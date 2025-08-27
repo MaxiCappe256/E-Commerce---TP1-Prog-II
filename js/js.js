38444fabef0b063196006fc806274250ed2e7d85
MaxiCappe256 <maxicappe256@gmail.com>
2025-08-27 09:59:42 -0300

    Add Function and Style Product hmtl

--- js/js.js
+++ js/js.js
@@ -1,20 +1,25 @@
 const hamburguesa = document.querySelector(".hamburguesa");
 const menu = document.querySelector('.navbar__links');
 const links = document.querySelectorAll('.navbar__link');
+const cruz = document.querySelector('.navbar__close');
 
 hamburguesa.addEventListener('click', () => {
     menu.classList.toggle('active');
+    cruz.classList.toggle('active');
 })
 
 
 links.forEach(link => {
     link.addEventListener('click', () => {
         menu.classList.remove('active');
+        cruz.classList.remove('active');
+
     })
 })
 
 document.addEventListener('click', (e) => {
     if(!menu.contains(e.target) && !hamburguesa.contains(e.target)) {
         menu.classList.remove('active');
+        cruz.classList.remove('active');
     }
 });
