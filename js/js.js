ca40ef4140ef12df5a69cfef6f8a7b5152cea942
ManuelBausela1 <bauselamanuel@gmail.com>
2025-09-05 17:00:41 -0300

    Part I cartshop

--- js/js.js
+++ js/js.js
@@ -2,6 +2,10 @@ const hamburguesa = document.querySelector(".hamburguesa");
 const menu = document.querySelector('.navbar__links');
 const links = document.querySelectorAll('.navbar__link');
 const cruz = document.querySelector('.navbar__close');
+const carrito = document.querySelector('#carrito');
+const vaciarCarritoBtn = document.querySelector('#vaciar-carrito'); 
+const agregarCarritoBtn = document.querySelector('#agregar-carrito');
+const productoContainerGeneral = document.querySelector('#producto-container-general');
 
 hamburguesa.addEventListener('click', () => {
     menu.classList.toggle('active');
@@ -23,3 +27,109 @@ document.addEventListener('click', (e) => {
         cruz.classList.remove('active');
     }
 });
+
+// Carrito 
+// Crear array vacio
+let articulosCarritos = [];
+
+function cargarEventListeners() {
+    // Cuando se presiona "Agregar al carrito"
+
+    productoContainerGeneral.addEventListener('click', agregarAlCarrito);
+
+    // Cuando se elimina un producto del carrito
+    carrito.addEventListener('click', eliminarProducto);
+
+    // Al vaciar el carrito
+    vaciarCarritoBtn.addEventListener('click', () => {
+        articulosCarritos = []; // Reseteamos el arreglo
+        limpiarHTML(); // Eliminamos todo el HTML
+    });
+}
+
+
+function agregarAlCarrito(e) {
+    // Evitar que la pagina se recargue al presionar el boton
+    e.preventDefault();
+
+    //Verificar si el usuario apreto en el boton
+    if(e.target.classList.contains('descripcion__comprar')) {
+        const productoSeleccionado = e.target.parentElement.parentElement;
+        leerDatosProducto(productoSeleccionado);
+    }
+}
+
+function leerDatosProducto(producto) {
+    // Crear un objeto con el contenido del producto actual
+    const infoProducto = {
+        imagen: producto.querySelector('img').src,
+        titulo: producto.querySelector('h3').textContent,
+        precio: producto.querySelector('.descripcion__precio').textContent,
+        id: producto.querySelector('a').getAttribute('data-id'),
+        cantidad: 1
+    }
+
+    // Revisar si un elemento ya existe en el carrito
+    const existe = articulosCarritos.some(producto => producto.id === infoProducto.id);
+
+    if(existe) {
+        const productos = articulosCarritos.map(producto => {
+            if(producto.id === infoProducto.id) {
+                producto.cantidad++;
+                return producto; // Retorna el objeto actualizado
+            }
+            else {
+                return producto; // Retorna los objetos que no son duplicados
+            }
+        });
+        articulosCarritos = [...productos];
+
+    }
+    else {
+        articulosCarritos = [...articulosCarritos, infoProducto];
+    }
+
+    imprimirCarrito();
+}
+
+function imprimirCarrito() {
+    
+    // Limpiar el HTML
+    limpiarHTML();
+
+    // Recorrer el carrito y generar el HTML
+    articulosCarritos.forEach(producto => {
+        const {imagen, titulo, precio, cantidad, id} = producto;
+
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td><img src="${imagen}" width="400" height="auto"></td>
+            <td>${titulo}</td>
+            <td>${precio}</td>
+            <td>${cantidad}</td>
+            <td>
+                <a href="#" class="borrar-producto" data-id="${id}"> X </a>
+            </td>
+        `;
+
+        carrito.appendChild(row);
+    });
+}
+
+function limpiarHTML() {
+    while(carrito.firstChild) {
+        carrito.removeChild(carrito.firstChild);
+    }
+}
+
+function eliminarProducto(e) {
+    e.preventDefault();
+    if(e.target.classList.contains('borrar-producto')) {
+        const productoId = e.target.getAttribute('data-id');
+
+        // Eliminar del arreglo de articulosCarrito por el data-id
+        articulosCarritos = articulosCarritos.filter(producto => producto.id !== productoId);
+
+        imprimirCarrito();
+    }
+}
--- js/product.js
+++ js/product.js
@@ -7,28 +7,31 @@ fetch("https://sneakers-api-cmkf.onrender.com/get-sneaker/" + productId)
     .then(product => {
         console.log(product);
         producto.innerHTML = `
-        <div class="container-img">
 
-            <div class="container-img__principal">
-                <a href="${product.image[0]}">
-                    <img class="container-img__imagen0" id="imgPrincipal" src="${product.image[0]}" alt="imagen principal">
-                </a>
+        <div class="container-product">
+            <div class="container-img">
+
+                <div class="container-img__principal">
+                    <a href="${product.image[0]}">
+                        <img class="container-img__imagen0" id="imgPrincipal" src="${product.image[0]}" alt="imagen principal">
+                    </a>
+                </div>
+
+                <div class="container-img__secundarias">
+                    ${product.image.slice(1).map((img, i) =>
+                `<a href="${img}">
+                            <img class="container-img__imagen-secundaria" src="${img}" alt="imagen zapatilla">
+                        </a>`).join("")}
+                </div>
             </div>
 
-            <div class="container-img__secundarias">
-                ${product.image.slice(1).map((img, i) =>
-            `<a href="${img}">
-                        <img class="container-img__imagen-secundaria" src="${img}" alt="imagen zapatilla">
-                    </a>`).join("")}
+            <div class="container__descripcion">
+                <h3 class="descripcion__titulo">${product.name}</h3>
+                <p class="descripcion__talle">${product.sizes.join(" - ")}</p>
+                <p class="descripcion__precio">$${product.price}.000</p>
+                <a href="../pages/carrito.html" class="descripcion__comprar" data-id="${product.id}" id="agregar-carrito" >Agregar Carrito</a>
             </div>
         </div>
-
-        <div class="container__descripcion">
-            <h3 class="descripcion__titulo">${product.name}</h3>
-            <p class="descripcion__talle">${product.sizes.join(" - ")}</p>
-            <p class="descripcion__precio">$${product.price}.000</p>
-            <a href="#" class="descripcion__comprar">Comprar</a>
-        </div>
-            `;
+        `;
         }
     )
