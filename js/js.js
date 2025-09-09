12e941fe58c315e34ebc3afb81e578d1d761772d
MaxiCappe256 <maxicappe256@gmail.com>
2025-09-09 16:58:36 -0300

    Cart, Dark Mode, etc.

--- js/js.js
+++ js/js.js
@@ -1,14 +1,29 @@
+// Nabvar
+
 const hamburguesa = document.querySelector(".hamburguesa");
 const menu = document.querySelector('.navbar__links');
 const links = document.querySelectorAll('.navbar__link');
 const cruz = document.querySelector('.navbar__close');
 
-const carritoContenedor = document.querySelector('.carrito-container');
-const iconoCarrito = document.querySelector('#icono-carrito');
 
-iconoCarrito.addEventListener('click', () => {
-    carritoContenedor.classList.toggle('active');
-})
+const modoOscuroBtn = document.querySelector('.modo-oscuro');
+const body = document.querySelector('body');
+const titulos = document.querySelectorAll('h2');
+const cards = document.querySelectorAll('.container__product');
+
+
+
+// Modo Oscuro
+modoOscuroBtn.addEventListener('click', () => {
+    body.classList.toggle('modo-oscuro-general');
+    titulos.forEach(titulo => {
+        titulo.classList.toggle('titulo-oscuro');
+    })
+    cards.forEach(card => {
+        // Cambia de add a toggle
+        card.classList.toggle('card-oscuro');
+    })
+} )
 
 hamburguesa.addEventListener('click', () => {
     menu.classList.toggle('active');
@@ -32,107 +47,71 @@ document.addEventListener('click', (e) => {
 });
 
 // Carrito 
-// Crear array vacio
-let articulosCarritos = [];
 
-function cargarEventListeners() {
-    // Cuando se presiona "Agregar al carrito"
+const carritoContenedor = document.querySelector('.carrito-container');
+const iconoCarrito = document.querySelector('#icono-carrito');
+const listaCarrito = document.querySelector('#lista-carrito');
+const vaciarCarritoBtn = document.querySelector('.vaciar-carrito-btn');
+const linkCarrito = document.querySelector('#link-carrito');
 
-    productoContainerGeneral.addEventListener('click', agregarAlCarrito);
-
-    // Cuando se elimina un producto del carrito
-    carrito.addEventListener('click', eliminarProducto);
-
-    // Al vaciar el carrito
-    vaciarCarritoBtn.addEventListener('click', () => {
-        articulosCarritos = []; // Reseteamos el arreglo
-        limpiarHTML(); // Eliminamos todo el HTML
-    });
-}
+let articulosCarrito = [];
 
 
-function agregarAlCarrito(e) {
-    // Evitar que la pagina se recargue al presionar el boton
+// Mostrar el carrito
+linkCarrito.addEventListener('click', (e) => {
     e.preventDefault();
+    carritoContenedor.classList.toggle('active')
+})
 
-    //Verificar si el usuario apreto en el boton
-    if(e.target.classList.contains('descripcion__comprar')) {
-        const productoSeleccionado = e.target.parentElement.parentElement;
-        leerDatosProducto(productoSeleccionado);
+
+iconoCarrito.addEventListener('click', (e) => {
+    e.preventDefault();
+    carritoContenedor.classList.toggle('active')
+})
+
+vaciarCarritoBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    articulosCarrito = [];
+    limpiarHTML();
+})
+
+function limpiarHTML() {
+    while (listaCarrito.firstChild) {
+        listaCarrito.removeChild(listaCarrito.firstChild);
     }
 }
 
-function leerDatosProducto(producto) {
-    // Crear un objeto con el contenido del producto actual
-    const infoProducto = {
-        imagen: producto.querySelector('img').src,
-        titulo: producto.querySelector('h3').textContent,
-        precio: producto.querySelector('.descripcion__precio').textContent,
-        id: producto.querySelector('a').getAttribute('data-id'),
-        cantidad: 1
-    }
-
-    // Revisar si un elemento ya existe en el carrito
-    const existe = articulosCarritos.some(producto => producto.id === infoProducto.id);
+function agregarAlCarrito(producto) {
+    const existe = articulosCarrito.some(p => p.id === producto.id)
 
     if(existe) {
-        const productos = articulosCarritos.map(producto => {
-            if(producto.id === infoProducto.id) {
-                producto.cantidad++;
-                return producto; // Retorna el objeto actualizado
+        articulosCarrito = articulosCarrito.map(p => {
+            if(p.id === producto.id) {
+                p.cantidad ++;
             }
-            else {
-                return producto; // Retorna los objetos que no son duplicados
-            }
-        });
-        articulosCarritos = [...productos];
-
-    }
-    else {
-        articulosCarritos = [...articulosCarritos, infoProducto];
+            return p;
+        })
+    } else {
+        const informacionProducto = {...producto, cantidad: 1};
+        articulosCarrito = [...articulosCarrito, informacionProducto];
     }
 
     imprimirCarrito();
 }
 
 function imprimirCarrito() {
-    
-    // Limpiar el HTML
     limpiarHTML();
 
-    // Recorrer el carrito y generar el HTML
-    articulosCarritos.forEach(producto => {
-        const {imagen, titulo, precio, cantidad, id} = producto;
+    articulosCarrito.forEach(producto => {
+        const {image, name, price, cantidad, id} = producto;
 
-        const row = document.createElement('tr');
-        row.innerHTML = `
-            <td><img src="${imagen}" width="400" height="auto"></td>
-            <td>${titulo}</td>
-            <td>${precio}</td>
-            <td>${cantidad}</td>
-            <td>
-                <a href="#" class="borrar-producto" data-id="${id}"> X </a>
-            </td>
+        const fila = document.createElement('tr');
+        fila.innerHTML = `
+        <td><img src="${image[0]}" width="100px" height="auto"></td> 
+        <td class="td-nombre">${name}</td>
+        <td class="td-precio">${price}</td>
+        <td class="td-cantidad">${cantidad}</td>
         `;
-
-        carrito.appendChild(row);
-    });
-}
-
-function limpiarHTML() {
-    while(carrito.firstChild) {
-        carrito.removeChild(carrito.firstChild);
-    }
-}
-
-function eliminarProducto(e) {
-    e.preventDefault();
-    if(e.target.classList.contains('borrar-producto')) {
-        const productoId = e.target.getAttribute('data-id');
-
-        // Eliminar del arreglo de articulosCarrito por el data-id
-        articulosCarritos = articulosCarritos.filter(producto => producto.id !== productoId);
-
-        imprimirCarrito();
-    }
+        listaCarrito.appendChild(fila);
+    })
 }
--- js/productos-index.js
+++ js/productos-index.js
@@ -13,9 +13,26 @@ fetch("https://sneakers-api-cmkf.onrender.com/")
                         <h3 class="info__titulo sub-subtitle">${products[i].name}</h3>
                         <p class="info__price">$${products[i].price}.000</p>
                         <a href="./pages/producto.html?id=${products[i].id}" target="_blank" class="info__btn">Ver más</a>
-                        <a href="./pages/producto.html?id=${products[i].id}" target="_blank" class="info__btn">Agregar al Carrito</a>
+                        <a href="#" data-id="${products[i].id}" class="info__btn btn-agregar">Agregar al Carrito</a>
                     </div>
                 </div>
             `
         }
+
+        // Saber que producto se clickeo
+        // Guardamos todos los botones en una variable
+        const botonesAgregar = document.querySelectorAll('.btn-agregar');
+        // Recorre todos los botones para ver cual es el clickeado
+        botonesAgregar.forEach(boton => {
+            boton.addEventListener('click', e => {
+                // Evita que el click suba al documento
+                e.preventDefault();
+                // Guardamos el id del boton que se apreto
+                const id = boton.getAttribute('data-id');
+                // Guardamos el producto con ese id buscandolo adentro del array
+                const producto = products.find(p => p.id == id);
+                // Llamamos a la funcion y le pasamos por parametro el producto que se clickeo
+                agregarAlCarrito(producto);
+            });
+        });
     })
