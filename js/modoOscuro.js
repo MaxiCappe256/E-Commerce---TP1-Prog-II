a8efb4167ee2e8394fea889aadff81d550e54be3
MaxiCappe256 <maxicappe256@gmail.com>
2025-09-19 14:03:55 -0300

    dark mode localstorage

--- js/modoOscuro.js
+++ js/modoOscuro.js
@@ -1,37 +1,15 @@
-// Modo Oscuro
-
-let modoOscuroGuardado = localStorage.getItem('modoOscuro');
 const modoOscuroBtn = document.querySelector('.modo-oscuro');
-const body = document.querySelector('body');
-const titulos = document.querySelectorAll('h2');
-const parrafos = document.querySelectorAll('p');
-const labels = document.querySelectorAll('label');
-const inputsDark = document.querySelectorAll('input');
-const legends = document.querySelectorAll('legend');
-const selectDark = document.querySelector('select');
+const body = document.body;
+
+const estiloGuardado = localStorage.getItem('modoOscuro');
+console.log(estiloGuardado)
+if (estiloGuardado === 'true') {
+  body.classList.add('modo-oscuro');
+} else {
+  body.classList.remove('modo-oscuro');
+}
 
 modoOscuroBtn.addEventListener('click', () => {
-    body.classList.toggle('modo-oscuro-general');
-    titulos.forEach(titulo => {
-        titulo.classList.toggle('titulo-oscuro');
-    })
-    const cards = document.querySelectorAll('.container__product');
-    cards.forEach(card => {
-        // Cambia de add a toggle
-        card.classList.toggle('card-oscuro');
-    })
-    parrafos.forEach(parrafo => {
-        parrafo.classList.toggle('parrafos-oscuros');
-    })
-    labels.forEach(label => {
-        label.classList.toggle('label-oscuro');
-    })
-    inputsDark.forEach(input => {
-        input.classList.toggle('input-oscuro');
-    })  
-    legends.forEach(legend => {
-        legend.classList.toggle('legend-oscuro');
-    })
-    selectDark.classList.toggle('select-oscuro');
-
-})
+  body.classList.toggle('modo-oscuro');
+  localStorage.setItem('modoOscuro', body.classList.contains('modo-oscuro'));
+});
